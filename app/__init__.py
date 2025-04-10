b4a489bab7397f8c48eb7d3506c919b2b1f30272
Rayon Hart <114370781+rayondhart@users.noreply.github.com>
2025-04-10 12:47:10 -0500

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
-from flask import Flask
+ from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
+from flask_wtf.csrf import CSRFProtect 
+
+import os
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+
+csrf = CSRFProtect(app)
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db) 
+
+app.config.from_object(Config)
+
 
 from app import views
