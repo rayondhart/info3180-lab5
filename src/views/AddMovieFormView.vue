40facedbcfd4af97e27ccd0b11b066033153dbe9
Rayon Hart <114370781+rayondhart@users.noreply.github.com>
2025-04-14 15:23:45 -0500

    excercise 4

---
+++ src/views/AddMovieFormView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Add Movie</h1>
+        <MovieForm />
+    </div>
+</template>
+
+
+<script>
+import MovieForm from "@/components/MovieForm.vue";
+
+export default {
+  components: {
+    MovieForm // Registering MovieForm component
+  }
+};
+</script>
